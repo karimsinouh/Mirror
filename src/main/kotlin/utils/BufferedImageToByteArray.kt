62d305bd73228ec63fa03052a4ded9204fde5eef
karim <sinouhkar@gmail.com>
2021-05-07 15:04:57 -0700

    [ADD extension function to convert BufferedImageToByteArray.kt

---
+++ src/main/kotlin/utils/BufferedImageToByteArray.kt
@@ -0,0 +1,11 @@
+package utils
+
+import java.awt.image.BufferedImage
+import java.io.ByteArrayOutputStream
+import javax.imageio.ImageIO
+
+private fun BufferedImage.toByteArray():ByteArray{
+    val baos= ByteArrayOutputStream()
+    ImageIO.write(this,"png",baos)
+    return baos.toByteArray()
+}
