c61b9b24234fcf14f6ec0adb0e9360420471cc48
karim <sinouhkar@gmail.com>
2021-05-07 15:00:50 -0700

    [ADD] load image from the url

---
+++ src/main/kotlin/ui/LoadImage.kt
@@ -0,0 +1,21 @@
+package ui
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.awt.image.BufferedImage
+import java.net.HttpURLConnection
+import java.net.URL
+import javax.imageio.ImageIO
+
+private suspend fun loadImage(source:String): BufferedImage? = withContext(Dispatchers.IO){
+    runCatching {
+        val url= URL(source)
+        val connection=url.openConnection() as HttpURLConnection
+        connection.connectTimeout=5000
+        connection.connect()
+
+        val input=connection.inputStream
+        val image= ImageIO.read(input)
+        image
+    }.getOrNull()
+}
