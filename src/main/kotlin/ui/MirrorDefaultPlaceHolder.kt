20215f0d865396bb04cbbe5f270d46e7ed256629
karim <sinouhkar@gmail.com>
2021-05-07 15:05:52 -0700

    [ADD] default placeholder

---
+++ src/main/kotlin/ui/MirrorDefaultPlaceHolder.kt
@@ -0,0 +1,23 @@
+package ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun DefaultPlaceholder(){
+    Box(
+        Modifier
+            .background(Color.Black.copy(0.2f))
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator(strokeWidth = 3.dp,color = Color.Black.copy(0.5f))
+    }
+}
